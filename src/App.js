9957060f45c62b00bc57a7e296feb474cd135339
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-11-30 20:57:42 -0700

    refactored with map in SmallImage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 // IMPORT useState
+import {useState} from 'react'
 import React from 'react';
 import './styles.css';
-// IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
+import BigImage from './components/BigImage';
+import SmallImage from './components/SmallImage';
+//still need imagesArr for App due to state variable intial value
+import imagesArr from './imageData'
 
 export default function App() {
   // USE useState TO CREATE  [bigImage, setBigImage] 
   // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+const [bigImage, setBigImage] = useState(imagesArr[0].img)
 
   // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
   // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
-
-  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
-  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
-
+const handleClick = (img) =>{
+  setBigImage(img)
+}
   return (
     <div className="App">
       <h1>Cities Of The World</h1>
       <div id="wrapper">
-        <div id="thumbnails">
-          {/* RENDER THE IMAGES ARRAY  */}
-        </div>
-        {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
-        {/* <img src="" id="bigimage" alt='bigImage'/> */}
+        <SmallImage
+          bigImage={bigImage}
+          handleClick={handleClick} />
+        <BigImage
+          bigImage={bigImage} />
       </div>
     </div>
   );
---
+++ src/components/BigImage.js
@@ -0,0 +1,7 @@
+const BigImage = ({bigImage}) => {
+    return (
+        <img src={bigImage} id="bigimage" alt='bigImage' />
+    // THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage
+    )
+}
+export default BigImage
