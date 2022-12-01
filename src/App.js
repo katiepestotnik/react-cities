eac1281c1e674d0b6f058d490c2f75cf2713eb46
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-11-30 21:23:23 -0700

    fix my SmallImage mess - should not map as not reusable

--- src/App.js
+++ src/App.js
@@ -10,20 +10,31 @@ import imagesArr from './imageData'
 export default function App() {
   // USE useState TO CREATE  [bigImage, setBigImage] 
   // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
-const [bigImage, setBigImage] = useState(imagesArr[0].img)
-
-  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
+  const [bigImage, setBigImage] = useState(imagesArr[0].img)
+    // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
   // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
 const handleClick = (img) =>{
   setBigImage(img)
 }
+const images = imagesArr.map((ele, idx) => {
+    return (
+      <SmallImage
+        src={ele.img}
+        key={idx}
+        alt={ele.city}
+        className={ele.img === bigImage ? 'thumb border' : 'thumb'}
+        handleClick={handleClick}
+      />
+    )
+  })
+
   return (
     <div className="App">
       <h1>Cities Of The World</h1>
       <div id="wrapper">
-        <SmallImage
-          bigImage={bigImage}
-          handleClick={handleClick} />
+        <div id="thumbnails">
+          {images}
+        </div>
         <BigImage
           bigImage={bigImage} />
       </div>
--- src/components/SmallImage.js
+++ src/components/SmallImage.js
@@ -1,22 +1,10 @@
-// IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
-import imagesArr from "../imageData"
-const SmallImage = ({ handleClick, bigImage }) => {
-      // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
-  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
-    const images = imagesArr.map((ele, idx)=>{
+const SmallImage = ({ handleClick, src, alt, className }) => {
         return(<img
-        key={idx}
-        src={ele.img}
-        alt={ele.city}
-        className={ele.img === bigImage ? 'thumb border': 'thumb'}
-        onClick={()=>handleClick(ele.img)}
+        src={src}
+        alt={alt}
+        className={className}
+        onClick={()=>handleClick(src)}
         >
         </img>)
-      })
-    return (
-        <div id="thumbnails">
-            {images}
-        </div>
-    )
 }
 export default SmallImage
